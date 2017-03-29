56244cccbb7625db4f1a00cf958deef14181d1ae
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-03-29 11:20:29 -0700

    Fixed typos

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,12 @@ import styles from './app.css';
 import logo from './imgs/logo.svg';
 
 const App = () => (
-    <dvi>
+    <div>
         <div className={styles.app}>
             <img src={logo} className={styles.rotate}/>
             <h1>Welcome to React!</h1>
         </div>
-    </dvi>
+    </div>
 );
 
 export default App;
