c86177f4479b8909ab32f90429bdfaef6b5cb4cb
Erik Nuber <erik.nuber@yahoo.com>
2017-04-29 14:53:14 -0700

    404 page created

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import School from './components/individual_school/individual_school';
 import About from './components/about_us/about';
 import rootReducer from './reducers/reducers_index';
 import LandingFormPaper from './components/landing_form/landing_form_paper';
+import NotFound from './components/notfoundpage/not_found'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {blueGrey300, green300} from 'material-ui/styles/colors';
 
@@ -39,7 +40,8 @@ const landingImage = {
 
 const paperForm = () => (
     <div>
-        <img className="landingImg" style={landingImage} src="images/college.jpg"/>
+        <img className="landingImg hidden-md-down mt-3" style={landingImage} src="images/college.jpg"/>
+        <img className="landingImg hidden-lg-up mt-3" style={landingImage} src="images/flagler_college.png"/>
         <LandingFormPaper className='landingForm' />
     </div>
 );
@@ -53,6 +55,7 @@ ReactDOM.render(
                     <Route path="home" component={MainPage}/>
                     <Route path="school/:id" component={School}/>
                     <Route path="about" component={About}/>
+                    <Route path="*" component={NotFound}/>
                 </Route>
             </Router>
         </Provider>
