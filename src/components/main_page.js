4bfd545b81e634806be67571a67e78db4008a5fe
fastexposure <fastexposure@yahoo.com>
2017-05-02 13:27:37 -0700

    added basic legend

--- src/components/main_page.js
+++ src/components/main_page.js
@@ -6,7 +6,7 @@ import SchoolList from './main_school_list/school_list'
 import PaperExampleSimple from './main_school_list/main_paper'
 import Map from './google_maps/map';
 import MapButton from './button_legend/legend';
-
+import Legend from './google_maps/legend';
 
 class drawerForm extends Component {
 
@@ -38,6 +38,7 @@ class drawerForm extends Component {
                 <MapButton />
                 <SchoolList id="schoolListShowing"/>
                 <Map id="mapShowing" className="hidden"/>
+                <Legend className="legend" />
             </div>
         );
     }
