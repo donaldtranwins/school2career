6a2684641abb75651b090c3bc92097c4cd54af2b
ninoj0seph <ninojoseph.tugade@gmail.com>
2017-05-10 16:11:17 -0700

    Create request.php

---
+++ dist/request.php
@@ -0,0 +1,16 @@
+<?php
+    spl_autoload_register(function ($class_name) {
+        require_once '../server/handlers/'.$class_name . '.php';
+    });
+    header::declare();
+    $clientRequest = isset($_GET['schid'])
+        ? new OneSchool()
+        : (
+        isset($_SERVER['CONTENT_TYPE']) && empty($_GET)
+            ? ($_SERVER['CONTENT_TYPE'] == 'application/json'
+                ? new FetchSchools(json_decode(file_get_contents('php://input'), true))
+                : new RequestError('Please configure application json!'))
+            : new RequestError('Invalid URL Parameters')
+        );
+    echo json_encode($clientRequest ->processRequest());
+?>
