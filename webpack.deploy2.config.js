806eba73e456321a84e2938371c91a870ac61b7d
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-05-11 15:41:53 -0700

    Working bundle - Non minified

---
+++ webpack.deploy2.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    context: resolve(__dirname, 'src'),
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js|.jsx?$/,
+                use: [ 'babel-loader' ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [ 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack-loader?bypassOnDebug' ]
+            }
+        ]
+    }
+};
