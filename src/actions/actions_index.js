a666a0d904ff643aa78db129f7917dd01dd6d3f3
Donald Tran <tran.donald@gmail.com>
2017-05-18 11:52:15 -0700

    Added baseurl for both live and localhost

--- src/actions/actions_index.js
+++ src/actions/actions_index.js
@@ -3,6 +3,11 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 import { FETCH_SCHOOLS, ONE_SCHOOL, SHOW_MAP, CENTER_COORDS, USER_INPUT, SCHOOL_IMAGE, MAP_BOUNDS_INPUT, SHOW_LOADER } from './actions_types';
 
+// // for live site
+// const BASE_URL = 'https://school2career.net/fetch_schools';
+// const ONESCHOOL_URL = 'https://school2career.net/one_school/id/';
+
+// for testing on localhost
 const BASE_URL = 'http://dev.school2career.net/fetch_schools';
 const ONESCHOOL_URL = 'http://dev.school2career.net/one_school/id/';
 
