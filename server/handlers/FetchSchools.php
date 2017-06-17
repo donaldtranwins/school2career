6bf6c76c8f3c8ffb00cf908c50d9ce6bf11653a5
Donald J. Tran <donaldtranwins@users.noreply.github.com>
2017-06-16 17:34:45 -0700

    Added back type casting to boolean for checkboxes

--- server/handlers/FetchSchools.php
+++ server/handlers/FetchSchools.php
@@ -45,20 +45,20 @@ class FetchSchools{
                 $queryEnd .= "s.tuition_out<$tuition_sanitized AND ";
             }
         }
-        if ($this->data['public'] === false){
+        if ((bool)$this->data['public'] === false){
             $queryEnd .=          "s.ownership<>1 AND ";
         }
-        if ($this->data['private'] === false){
+        if ((bool)$this->data['private'] === false){
             $queryEnd .=          "s.ownership=1 AND ";
         }
-        if ($this->data['voc'] === false){
+        if ((bool)$this->data['voc'] === false){
             $queryEnd .=      "s.vocational=0 AND ";
         }
-        if ($this->data['aa'] === false){
+        if ((bool)$this->data['aa'] === false){
             array_push($tables, "pts", 'programs');
             $queryEnd .=      "pts.deg_2=0 AND ";
         }
-        if ($this->data['bs'] === false){
+        if ((bool)$this->data['bs'] === false){
             array_push($tables, "pts", 'programs');
             $queryEnd .=      "pts.deg_4=0 AND ";
         }
