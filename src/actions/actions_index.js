e9a198f641bd2d82f4d73d2858623f79453fa17b
Erik Nuber <erik.nuber@yahoo.com>
2017-06-18 18:19:13 -0700

    rebundle and branch off dev

--- src/actions/actions_index.js
+++ src/actions/actions_index.js
@@ -3,19 +3,15 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 import { FETCH_SCHOOLS, ONE_SCHOOL, SHOW_MAP, CENTER_COORDS, USER_INPUT, SCHOOL_IMAGE, MAP_BOUNDS_INPUT, SHOW_LOADER } from './actions_types';
 
-// // for live site
-const BASE_URL = 'https://school2career.net/fetch_schools';
-const ONESCHOOL_URL = 'https://school2career.net/one_school/id/';
+// // live site
+// const BASE_URL = 'https://school2career.net/fetch_schools';
+// const ONESCHOOL_URL = 'https://school2career.net/one_school/id/';
 
-// // for testing on localhost
-// const BASE_URL = 'http://dev.school2career.net/fetch_schools';
-// const ONESCHOOL_URL = 'http://dev.school2career.net/one_school/id/';
+// dev environment, and testing on localhost
+const BASE_URL = 'http://s2c.donaldjtran.com/fetch_schools';
+const ONESCHOOL_URL = 'http://s2c.donaldjtran.com/one_school/id/';
 
-// for testing on localhost, pointed to staging
-// const BASE_URL = 'http://s2c.donaldjtran.com/fetch_schools';
-// const ONESCHOOL_URL = 'http://s2c.donaldjtran.com/one_school/id/';
-
-// // for live site alternative
+// // staging environment
 // const BASE_URL = 'https://school.donaldjtran.com/fetch_schools';
 // const ONESCHOOL_URL = 'https://school.donaldjtran.com/one_school/id/';
 
