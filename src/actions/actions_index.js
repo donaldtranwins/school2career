33ebc305e909b7e1290b88c05ec15629d8d40415
Erik Nuber <erik.nuber@yahoo.com>
2017-06-18 17:19:33 -0700

    removed 0/1 info from last push per backend

--- src/actions/actions_index.js
+++ src/actions/actions_index.js
@@ -22,23 +22,6 @@ const ONESCHOOL_URL = 'http://s2c.donaldjtran.com/one_school/id/';
 //calls the database via axios to get all schools available
 export function searchForSchools(value) {
     value.newInfo = true;
-    if (value.aa !== undefined) {
-        if (value.aa === false) {
-            value.aa = 0;
-        } else {
-            value.aa = 1;
-        }
-        if (value.voc === false) {
-            value.voc = 0;
-        } else {
-            value.voc = 1;
-        }
-        if (value.bs === false) {
-            value.bs = 0;
-        } else {
-            value.bs = 1;
-        }
-    }
     const newVal = JSON.stringify(value);
     const request = axios.post(`${BASE_URL}`, newVal);
     return {
